66b20e8cd8a90fe2c0009e7a2b23ec5d25193f53
João Vítor P. Santos <joaovitorps00@gmail.com>
2024-05-22 17:20:33 -0300

    change deploy config from server file to app file since express variable is used

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const TaskRoutes = require("./routes/task");
 
 const app = express();
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build", index.html));
+  });
+}
+
 app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
 
--- server.js
+++ server.js
@@ -3,14 +3,6 @@ const database = require("./db/models/index");
 const app = require("./app");
 const PORT = process.env.PORT || 8000;
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client/build", index.html));
-  });
-}
-
 try {
   database.sequelize
     .authenticate()
